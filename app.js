972a7ad9fd7a4fad3ea8b62022f59a3beb8d65dd
alikamatu_devrel <122170675+alikamatu@users.noreply.github.com>
2025-06-24 23:09:05 +0000

    db models setup

--- app.js
+++
@@ -1,5 +0,0 @@
-MONGO_URI=mongodb+srv://<user>:<pass>@cluster0.mongodb.net/synchora
-REDIS_URL=redis://127.0.0.1:6379
-JWT_SECRET=your_secure_secret
-SMS_PROVIDER=africastalking
-AFRICASTALKING_API_KEY=your_api_key
